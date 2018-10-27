71b859e7ad12215cfc17bd10b6131c1e12efeeed
Matthew Kelly <badgerati42@gmail.com>
2018-10-27 16:55:09 +0100

    more setting defaults

--- config/settings.js
+++ config/settings.js
@@ -1,14 +1,14 @@
 module.exports = {
-    port: (process.env.PARROT_PORT || 8080),
+    port: (process.env.PARROT_PORT || process.env.PORT || 8080),
 
     locale: 'en-GB',
 
-    host_url: (process.env.PARROT_HOST_URL || 'http://localhost:8080'),
+    host_url: (process.env.PARROT_HOST_URL || process.env.HOST_URL || 'http://localhost:8080'),
 
-    env: (process.env.PARROT_NODE_ENV || 'dev'),
+    env: (process.env.PARROT_NODE_ENV || process.env.NODE_ENV || 'dev'),
 
     mongo: {
-        connection: (process.env.PARROT_MONGO_CONN || 'mongodb://mongo:27017/parrots')
+        connection: (process.env.PARROT_MONGO_CONN || process.env.MONGO_CONN || 'mongodb://mongo:27017/parrots')
     },
 
     encryption: {
@@ -26,7 +26,7 @@ module.exports = {
     },
 
     session: {
-        secret: (process.env.PARROT_SECRET || 'RickSanchez'),
+        secret: (process.env.PARROT_SECRET || process.env.SECRET || 'RickSanchez'),
         maxAge: 60000,
         resave: true,
         saveUninitialized: false
