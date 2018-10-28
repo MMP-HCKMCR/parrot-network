113568a3edb82785970789596dd969ff5348d1a3
Matthew Kelly <badgerati42@gmail.com>
2018-10-28 01:39:57 +0000

    hook up login and register

--- helpers/passport_init.js
+++ helpers/passport_init.js
@@ -15,50 +15,56 @@ module.exports = function(passport) {
     });
 
 
-    passport.use('login', new PassportLocal({
-        passReqToCallback: true
-    },
-    function(req, username, password, done) {
-        UserRepo.findByUsername(username, (err, user) => {
-            if (err) {
-                console.log(err);
-                return done(null, false, { message: err });
-            }
+    passport.use('login', new PassportLocal(
+        function(username, password, done) {
+            UserRepo.findByUsername(username, (err, user) => {
+                if (err) {
+                    console.log(err);
+                    return done(null, false, { message: err });
+                }
 
-            if (!user) {
-                return done(null, false, { message: 'Invalid username' });
-            }
+                if (!user) {
+                    console.log('[LOGIN] user not found');
+                    return done(null, false, { message: 'Invalid username' });
+                }
 
-            if (!user.verifyPasswordSync(password)) {
-                return done(null, false, { message: 'Invalid password' });
-            }
+                if (!user.verifyPasswordSync(password)) {
+                    console.log('[LOGIN] invalid password')
+                    return done(null, false, { message: 'Invalid password' });
+                }
 
-            return done(null, user);
-        })
-    }));
+                return done(null, user);
+            })
+        }
+    ));
+
 
     passport.use('signup', new PassportLocal({
         passReqToCallback: true
     },
     function(req, username, password, done) {
         var email = req.body.email;
-        var confirmPassword = req.email.confirmPassword
+        var confirmPassword = req.body.confirmPassword
         var fullname = req.body.fullname;
         //var dob = req.body.dob;
 
         if (!username || validator.isEmpty(username)) {
+            console.log('[SIGNUP] invalid username');
             return done(null, false, { message: 'Invalid username' });
         }
 
         if (!email || validator.isEmpty(email)) {
+            console.log('[SIGNUP] invalid email');
             return done(null, false, { message: 'Invalid email address' });
         }
 
         if (!fullname || validator.isEmpty(fullname)) {
+            console.log('[SIGNUP] invalid fullname');
             return done(null, false, { message: 'Invalid fullname' });
         }
 
         if (password != confirmPassword) {
+            console.log('[SIGNUP] passwords do not match');
             return done(null, false, { message: 'Passwords do not match' });
         }
 
@@ -69,6 +75,7 @@ module.exports = function(passport) {
             }
 
             if (user) {
+                console.log('[SIGNUP] username taken');
                 return done(null, false, { message: 'Username already taken' });
             }
 
